57b9ee72b54be661f2f249a2baa916416fd59ef2
Alex <alexbachurin91@gmail.com>
2021-09-06 19:40:15 +0300

    show more toggle button

--- src/Tour.js
+++ src/Tour.js
@@ -1,8 +1,15 @@
-import React, { useState } from 'react'
+import React, { useEffect, useState } from 'react'
 
 export default function Tour({ id, image, name, price, info }) {
-
-
+    //show more
+    const [readMore, setReadMore] = useState(false);
+    //slice our info property to not fully show description, only part of it
+    const slicedInfo = `${info.slice(0, 155)}...`
+    //function to show info
+    const showInfo = () => {
+        //toggle info depending on current readmore state if its true,then put it in false and reverse
+        setReadMore(!readMore);
+    }
     return (
         <article className="single-tour">
             <img src={image} alt={name} />
@@ -14,8 +21,14 @@ export default function Tour({ id, image, name, price, info }) {
                     </h4>
                 </div>
                 <p>
-                    {info}
-                    <button>read more</button>
+                    {/* if readMore is in false state we show only sliced info
+                    else if user pressed "Read more" we show full info */}
+                    {readMore ? `${info}` : `${slicedInfo}`}
+                    <button onClick={showInfo}>
+                        {/* if readMore is in false state, we show text as 'Read More'
+                        else we show it as 'Show Less' */}
+                        {readMore ? 'Show Less' : 'Read More'}
+                    </button>
                 </p>
                 <button className="delete-btn">not interested</button>
             </footer>
